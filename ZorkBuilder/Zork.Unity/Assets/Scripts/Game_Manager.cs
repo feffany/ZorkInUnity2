6529c1743491c977f7219578524045e972842b04
feffany <48835773+feffany@users.noreply.github.com>
2020-12-09 12:04:53 -0500

    Initial Commit

---
+++ ZorkBuilder/Zork.Unity/Assets/Scripts/Game_Manager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zork;
+
+public class Game_Manager : MonoBehaviour
+{
+    public string ZorkFilename = @"Assets\Resources\Zork.json";
+    public UnityOutputService OutputService;
+    public UnityInputService InputService;
+
+    void Awake()
+    {
+        //TextAsset gameJsonAsset = Resources.Load<TextAsset>(ZorkGameFileAssetName);
+        //Game.Start(gameJsonAsset.text, InputService, OutputService);
+        //Game.Instance.CommandManager.PerformCommand(Game.Instance, "LOOK");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextAsset gameJsonAsset = Resources.Load<TextAsset>(ZorkGameFileAssetName);
+        Game.Start(gameJsonAsset.text, InputService, OutputService);
+        Game.Instance.CommandManager.PerformCommand(Game.Instance, "LOOK");
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        if (Input.GetKey(KeyCode.Return))
+        {
+            InputService.ProcessInput();
+        }
+    }*/
+
+    [SerializeField]
+    private string ZorkGameFileAssetName = "Zork";
+}
---
+++ ZorkBuilder/Zork.Unity/Assets/Scripts/UnityOutputService.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Zork;
+using TMPro;
+
+public class UnityOutputService : MonoBehaviour, IOutputService
+{
+    [SerializeField]
+    private TextMeshProUGUI TextLinePrefab;
+
+    [SerializeField]
+    private Transform OutputTextContainer;
+
+    [SerializeField]
+    private int MaxTextLines = 60;
+
+    public UnityOutputService()
+    {
+        mTextLines = new List<GameObject>();
+    }
+
+    public void Clear()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Write(string value)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Write(object value)
+    {
+        Write(value.ToString());
+    }
+
+    public void WriteLine(string value)
+    {
+        //OutputText.text = value;
+        if(mTextLines.Count >= MaxTextLines)
+        {
+            Destroy(mTextLines[0]);
+            mTextLines.RemoveAt(0);
+        }
+
+        var textLine = Instantiate(TextLinePrefab);
+        textLine.transform.SetParent(OutputTextContainer, false);
+        textLine.text = value;
+        mTextLines.Add(textLine.gameObject);
+    }
+
+    public void WriteLine(object value)
+    {
+        WriteLine(value.ToString());
+    }
+
+    private List<GameObject> mTextLines;
+}
